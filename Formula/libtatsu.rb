4d3919638cad42cc0ce73ab7841a681710354dc0
Zhenhao Qi <66653498+zqidev@users.noreply.github.com>
2024-08-27 23:58:27 -0700

    Update libtatsu.rb

--- Formula/libtatsu.rb
+++ Formula/libtatsu.rb
@@ -5,6 +5,9 @@ class Libtatsu < Formula
   sha256 "4f69452d23e50e0ffbe844110e6ab6a900d080e051fbda3b7d595f679dee9bc5"
   version "1.0.3"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "libplist"
   depends_on "openssl@3"
@@ -13,7 +16,7 @@ class Libtatsu < Formula
     ENV["libcurl_CFLAGS"] = "-I#{`xcrun --sdk macosx --show-sdk-path`.strip}/usr/include"
     ENV["libcurl_LIBS"] = "-lcurl"
 
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", *std_configure_args
     system "make"
     system "make", "install"
 
